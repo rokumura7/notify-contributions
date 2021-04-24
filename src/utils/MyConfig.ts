b5814c7db08bd54c4fa50e4b510fa4f00b35162a
rokumura7 <rokumura.8807@gmail.com>
2021-04-24 22:38:33 +0900

    fixed my config

--- src/utils/MyConfig.ts
+++ src/utils/MyConfig.ts
@@ -1,13 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const isProd = process.argv[2] === 'prod';
-
 const config = {
-  slackUrl: isProd ? process.argv[3] : process.env.SLACK_URL,
-  slackChannel: isProd ? process.argv[4] : process.env.SLACK_CHANNEL,
-  githubToken: isProd ? process.argv[5] : process.env.GITHUB_TOKEN,
-  username: isProd ? process.argv[6] : process.env.USERNAME,
+  slackUrl: process.env.SLACK_URL,
+  slackChannel: process.env.SLACK_CHANNEL,
+  githubToken: process.env.GITHUB_TOKEN,
+  username: process.env.USERNAME,
 };
 
 export default config;
